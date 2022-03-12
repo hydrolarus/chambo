efd8c622c8b9a7a6488a8ab9ad44df83e16615f6
cuddlefishie <lara.fishie@gmail.com>
2022-03-12 14:09:15 +0100

    format with stylish-haskell

--- app/Main.hs
+++ app/Main.hs
@@ -1,176 +1,72 @@
 module Main where
 
-import Control.Monad (forM_, when)
-import Data.Maybe (listToMaybe)
-import Verification
-import Syntax
-import System.Environment (getArgs)
-import System.Exit (exitFailure)
-import Typecheck
-import Z3.Monad ( evalZ3 )
-import Parsing
-import Data.List (intercalate)
+import           Control.Monad       (forM_, when)
+import           Data.List           (intercalate)
+import           Data.Maybe          (listToMaybe)
+import           System.Console.ANSI
+import           System.Environment  (getArgs)
+import           System.Exit         (exitFailure)
 
-import System.Console.ANSI
+import           Parsing
+import           Syntax
+import           Typecheck
+import           Verification
 
-infixr 2 ==>
 
-infix 4 .&&
-
-infix 6 .>=
-
-infix 6 .<
-
-infix 6 .<=
-
-(.&&) :: Expr -> Expr -> Expr
-(.&&) = EBinOp OpAnd
-
-(.>=) :: Expr -> Expr -> Expr
-(.>=) = EBinOp OpGte
-
-(.<) :: Expr -> Expr -> Expr
-(.<) = EBinOp OpLt
-
-(.<=) :: Expr -> Expr -> Expr
-(.<=) = EBinOp OpLte
-
-(==>) :: Expr -> Expr -> Expr
-(==>) = EBinOp OpImplies
-
-(.==) :: Expr -> Expr -> Expr
-(.==) = EBinOp OpEq
-
-infixl 9 .*
-
-infixl 7 .+
-
-(.+) :: Expr -> Expr -> Expr
-(.+) = EBinOp OpAdd
-
-(.*) :: Expr -> Expr -> Expr
-(.*) = EBinOp OpMul
-
-var :: String -> Expr
-var = EVar
-
-int :: Integer -> Expr
-int = EIntLiteral
-
-t :: Expr -> Term
-t e = TExpr e Nothing
-
-fun name args ret pres posts body = Function name args ret pres posts body Nothing
-
-maxFn :: Function
-maxFn =
-  fun
-    "max"
-    [("a", TInt), ("b", TInt)]
-    TInt
-    []
-    [ TDisjunction (t $ var "result" .== var "a") (t $ var "result" .== var "b"),
-      TConjunction (t $ var "result" .>= var "a") (t $ var "result" .>= var "b")
-    ]
-    ( EIf (var "a" .>= var "b") (var "a") (var "b")
-    )
-
-sem :: Expr -> Expr -> Expr
-sem = ESeq
-
-testFn :: Function
-testFn =
-  fun "test" [] TUnit [] [] $
-    ELet "res" (EFunCall "max" [("b", int 2), ("a", int 5)]) $
-      EAssert (t $ var "res" .== int 4)
-        `sem` EUnitLiteral
-
-vcTest :: Function
-vcTest =
-  fun
-    "vc_test"
-    []
-    TUnit
-    []
-    [ -- trivial
-      t $ int 3 .<= int 5,
-      -- WRONG!
-      -- forall x. 0 < x <= 6  ==> x <= 2
-      TForall "x" TInt . t $ (int 0 .< var "x" .&& var "x" .<= int 6) ==> var "x" .<= int 2,
-      -- Correct!
-      -- forall x. 0 < x <= 6  ==> x <= 10
-      TForall "x" TInt . t $ (int 0 .< var "x" .&& var "x" .<= int 6) ==> var "x" .<= int 10
-    ]
-    EUnitLiteral
-
-assumptionsTest :: Function
-assumptionsTest =
-  fun
-    "assumptions"
-    [("a", TInt), ("b", TInt)]
-    TInt
-    -- preconditions
-    [ t $ var "a" .< var "b",
-      t $ int 0 .<= var "a",
-      t $ int 0 .<= var "b"
-    ]
-    -- posconditions
-    [t $ var "a" .< var "b" .* int 2]
-    (EIf (var "a" .== int 2) (int 3) (int 9))
-
-file :: File
-file = File $ map IFunction [maxFn, testFn]
+import           Z3.Monad            (evalZ3)
 
 getFile :: IO File
 getFile = do
-  fileName <- listToMaybe <$> getArgs
-  content <- maybe (pure "") readFile fileName
-  pure $ parse content
+    fileName <- listToMaybe <$> getArgs
+    content <- maybe (pure "") readFile fileName
+    pure $ parse content
 
 main :: IO ()
 main = do
-  file <- getFile
+    file <- getFile
 
-  putStr "Typechecking...  "
-  file' <- case runTypeCheckNew (processFile file) of
-    Left err -> do
-      putStrLn $ "\nFailed: " ++ show err
-      exitFailure
-    Right file -> do
-      putStrLn "Ok!"
-      pure file
+    putStr "Typechecking...  "
+    file' <- case runTypeCheckNew (processFile file) of
+        Left err -> do
+            putStrLn $ "\nFailed: " ++ show err
+            exitFailure
+        Right file -> do
+            putStrLn "Ok!"
+            pure file
 
-  putStrLn "\nRunning VCs..."
-  let vcs = allVCs file'
-  -- print vcs
-  forM_ (zip [1..] vcs) $ \(i, vc) -> do
-    res <- evalZ3 $ verify file' vc
+    putStrLn "\nRunning VCs..."
+    let vcs = allVCs file'
 
-    let path' = intercalate "." (path vc)
-    case res of
-      Unknown -> do
-        withColour Yellow $ putStr "Unknown "
-        putStrLn path'
-      Success -> do
-        withColour Green $ putStr "Success "
-        putStrLn path'
-      Failed model vc -> do
-        withColour Red $ putStr "Failed  "
-        putStrLn path'
+    forM_ (zip [1..] vcs) $ \(i, vc) -> do
+        res <- evalZ3 $ verify file' vc
 
-        when (model /= []) $
-          putStrLn $ "  Counterexample: " ++ show model
+        let path' = intercalate "." (path vc)
+        case res of
+            Unknown -> do
+                withColour Yellow $ putStr "Unknown "
+                putStrLn path'
 
-        putStrLn $ unlines . map ("  " ++) $ printVC vc
+            Success -> do
+                withColour Green $ putStr "Success "
+                putStrLn path'
 
-        pure ()
+            Failed model vc -> do
+                withColour Red $ putStr "Failed  "
+                putStrLn path'
+
+                when (model /= []) $
+                    putStrLn $ "  Counterexample: " ++ show model
+
+                putStrLn $ unlines . map ("  " ++) $ printVC vc
+
+                pure ()
 
 
 printVC :: VerificationCondition -> [String]
 printVC VC {..} = assumptions' ++ ["check:  " ++ prettyTerm expression]
-  where
-    -- assumptions' = flip map assumptions $ \s -> "assume: " ++ prettyTerm s
-    assumptions' = []
+    where
+        -- assumptions' = flip map assumptions $ \s -> "assume: " ++ prettyTerm s
+        assumptions' = []
 
 withColour :: Color -> IO a -> IO a
 withColour col action = do
--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,90 +1,89 @@
 module Syntax where
 
-import Data.List
-import qualified Data.Map as M
-import Data.Maybe
-import Data.Bifunctor
+import           Data.Bifunctor
+import           Data.List
+import qualified Data.Map       as M
+import           Data.Maybe
 
 newtype File = File [Item]
 
-newtype Item
-  = IFunction Function
-  deriving (Show, Eq)
+newtype Item = IFunction Function
+    deriving (Show, Eq)
 
 data Function = Function
-  { funName :: String,
-    funArgs :: [(String, Type)],
-    funRet :: Type,
-    funPre :: [Term],
-    funPost :: [Term],
-    funBody :: Expr,
-    funTBody :: Maybe TExpr
-  }
-  deriving (Show, Eq)
+    { funName  :: String
+    , funArgs  :: [(String, Type)]
+    , funRet   :: Type
+    , funPre   :: [Term]
+    , funPost  :: [Term]
+    , funBody  :: Expr
+    , funTBody :: Maybe TExpr
+    }
+    deriving (Show, Eq)
 
 data Type
-  = TInt
-  | TString
-  | TBool
-  | TUnit
-  deriving (Eq, Show)
+    = TInt
+    | TString
+    | TBool
+    | TUnit
+    deriving (Eq, Show)
 
 data Term
-  = TExpr Expr (Maybe TExpr)
-  | TForall String Type Term
-  | TDisjunction Term Term
-  | TConjunction Term Term
-  deriving (Show, Eq)
+    = TExpr Expr (Maybe TExpr)
+    | TForall String Type Term
+    | TDisjunction Term Term
+    | TConjunction Term Term
+    deriving (Show, Eq)
 
 data Expr
-  = EIntLiteral Integer
-  | EStringLiteral String
-  | EBoolLiteral Bool
-  | EUnitLiteral
-  | EBinOp BinOp Expr Expr
-  | EPrefixOp PrefixOp Expr
-  | ESeq Expr Expr
-  | EVar String
-  | EFunCall String [(String, Expr)]
-  | EAssert Term
-  | EIf Expr Expr Expr
-  | ELet String Expr Expr
-  deriving (Show, Eq)
+    = EIntLiteral Integer
+    | EStringLiteral String
+    | EBoolLiteral Bool
+    | EUnitLiteral
+    | EBinOp BinOp Expr Expr
+    | EPrefixOp PrefixOp Expr
+    | ESeq Expr Expr
+    | EVar String
+    | EFunCall String [(String, Expr)]
+    | EAssert Term
+    | EIf Expr Expr Expr
+    | ELet String Expr Expr
+    deriving (Show, Eq)
 
 data BinOp
-  = OpAdd
-  | OpSub
-  | OpMul
-  | OpDiv
-  | OpEq
-  | OpNeq
-  | OpLt
-  | OpLte
-  | OpGt
-  | OpGte
-  | OpImplies
-  | OpAnd
-  | OpOr
-  deriving (Show, Eq)
+    = OpAdd
+    | OpSub
+    | OpMul
+    | OpDiv
+    | OpEq
+    | OpNeq
+    | OpLt
+    | OpLte
+    | OpGt
+    | OpGte
+    | OpImplies
+    | OpAnd
+    | OpOr
+    deriving (Show, Eq)
 
 data PrefixOp
-  = OpNot
-  deriving (Show, Eq)
+    = OpNot
+    deriving (Show, Eq)
 
 data TExpr
-  = TEIntLiteral Integer
-  | TEStringLiteral String
-  | TEBoolLiteral Bool
-  | TEUnitLiteral
-  | TEBinOp BinOp TExpr TExpr
-  | TEPrefixOp PrefixOp TExpr
-  | TESeq TExpr TExpr
-  | TEVar String Type
-  | TEFunCall String [(String, TExpr)]
-  | TEAssert Term
-  | TEIf TExpr TExpr TExpr
-  | TELet String Type TExpr TExpr
-  deriving (Show, Eq)
+    = TEIntLiteral Integer
+    | TEStringLiteral String
+    | TEBoolLiteral Bool
+    | TEUnitLiteral
+    | TEBinOp BinOp TExpr TExpr
+    | TEPrefixOp PrefixOp TExpr
+    | TESeq TExpr TExpr
+    | TEVar String Type
+    | TEFunCall String [(String, TExpr)]
+    | TEAssert Term
+    | TEIf TExpr TExpr TExpr
+    | TELet String Type TExpr TExpr
+    deriving (Show, Eq)
 
 weakenExpr :: TExpr -> Expr
 weakenExpr (TEIntLiteral n) = EIntLiteral n
@@ -102,15 +101,15 @@ weakenExpr (TELet s ty rhs body) = ELet s (weakenExpr rhs) (weakenExpr body)
 
 lookupFunction :: File -> String -> Maybe Function
 lookupFunction (File items) name = go items
-  where
-    go [] = Nothing
-    go ((IFunction f@(Function n _ _ _ _ _ _)) : xs)
-      | n == name = Just f
-      | otherwise = go xs
+    where
+        go [] = Nothing
+        go ((IFunction f@(Function n _ _ _ _ _ _)) : xs)
+            | n == name = Just f
+            | otherwise = go xs
 
 functions :: File -> [Function]
 functions (File items) = flip mapMaybe items $
-  \case IFunction func -> Just func
+    \case IFunction func -> Just func
 
 substTExpr :: M.Map String TExpr -> TExpr -> TExpr
 substTExpr subst e@(TEIntLiteral n) = e
@@ -138,37 +137,35 @@ prettyTExpr (TEStringLiteral s) = show s
 prettyTExpr (TEBoolLiteral b) = show b
 prettyTExpr TEUnitLiteral = "()"
 prettyTExpr (TEBinOp op a b) = "(" ++ prettyTExpr a ++ " " ++ op' ++ " " ++ prettyTExpr b ++ ")"
-  where
-    op' = case op of
-      OpAdd -> "+"
-      OpSub -> "-"
-      OpMul -> "*"
-      OpDiv -> "/"
-      OpEq -> "="
-      OpNeq -> "!="
-      OpLt -> "<"
-      OpLte -> "<="
-      OpGt -> ">"
-      OpGte -> ">="
-      OpImplies -> "==>"
-      OpAnd -> "&&"
-      OpOr -> "||"
+    where
+        op' = case op of
+            OpAdd     -> "+"
+            OpSub     -> "-"
+            OpMul     -> "*"
+            OpDiv     -> "/"
+            OpEq      -> "="
+            OpNeq     -> "!="
+            OpLt      -> "<"
+            OpLte     -> "<="
+            OpGt      -> ">"
+            OpGte     -> ">="
+            OpImplies -> "==>"
+            OpAnd     -> "&&"
+            OpOr      -> "||"
 prettyTExpr (TEPrefixOp op a) = op' ++ "(" ++ prettyTExpr a ++ ")"
-  where
-    op' = case op of
-      OpNot -> "!"
+    where
+        op' = case op of
+            OpNot -> "!"
 prettyTExpr (TESeq a b) = prettyTExpr a ++ "; " ++ prettyTExpr b
 prettyTExpr (TEVar s ty) = s
 prettyTExpr (TEFunCall name args) = name ++ "(" ++ intercalate ", " args' ++ ")"
-  where
-    args' = map (\(name, expr) -> name ++ ": " ++ prettyTExpr expr) args
+    where
+        args' = map (\(name, expr) -> name ++ ": " ++ prettyTExpr expr) args
 prettyTExpr (TEAssert te) = "assert { " ++ prettyTerm te ++ " }"
 prettyTExpr (TEIf cond thn els) =
-  "if " ++ prettyTExpr cond
-    ++ " then "
-    ++ prettyTExpr thn
-    ++ " else "
-    ++ prettyTExpr els
+    "if " ++ prettyTExpr cond
+      ++ " then " ++ prettyTExpr thn
+      ++ " else " ++ prettyTExpr els
 prettyTExpr (TELet name ty rhs body) =
   "let " ++ name ++ " : " ++ prettyType ty
     ++ " = "
@@ -184,7 +181,7 @@ prettyTerm (TDisjunction a b) = "(" ++ prettyTerm a ++ " \\/ " ++ prettyTerm b +
 prettyTerm (TConjunction a b) = "(" ++ prettyTerm a ++ " /\\ " ++ prettyTerm b ++ ")"
 
 prettyType :: Type -> String
-prettyType TInt = "int"
+prettyType TInt    = "int"
 prettyType TString = "string"
-prettyType TBool = "bool"
-prettyType TUnit = "()"
+prettyType TBool   = "bool"
+prettyType TUnit   = "()"
